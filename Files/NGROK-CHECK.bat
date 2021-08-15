087cad6f52123a183440892b9c40dd290466b06d
kzinthant-kas <88583919+kzinthant-kas@users.noreply.github.com>
2021-08-15 15:58:16 +0530

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,11 +1,11 @@
 @echo off
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user Administrator kzinthant123 /add >nul
-net localgroup administrators administrator /add >nul
+net user kzinthant kzinthant /add >nul
+net localgroup administrators kzinthant /add >nul
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Administrator
+echo User: kzinthant
 echo Pass:  
 curl -O DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" FastConfigVPS_v5.1.exe > out.txt 2>&1
@@ -17,6 +17,6 @@ PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComp
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant kzinthant:F >nul
+ICACLS C:\Windows\installer /grant kzinthant:F >nul
 ping -n 10 127.0.0.1 >nul
